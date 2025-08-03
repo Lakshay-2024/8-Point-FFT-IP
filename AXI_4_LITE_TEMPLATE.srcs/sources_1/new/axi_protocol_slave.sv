9d1548a867c3566100aa1e030dd37a82f3125fc6
Lakshay-2024 <157269595+Lakshay-2024@users.noreply.github.com>
2025-08-03 11:24:32 +0530

    Update axi_protocol_slave.sv

--- AXI_4_LITE_TEMPLATE.srcs/sources_1/new/axi_protocol_slave.sv
+++ AXI_4_LITE_TEMPLATE.srcs/sources_1/new/axi_protocol_slave.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/27/2025 08:39:23 PM
-// Design Name: 
-// Module Name: axi_protocol_slave
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module axi_protocol_slave(
